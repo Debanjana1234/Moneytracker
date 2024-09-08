56c72c1ba88ec3282b8d88e35b7d5bdab07b41d9
Debanjana1234 <nandandebanjana@gmail.com>
2024-09-08 17:24:59 +0530

    Initial commit

---
+++ moneytracker/api/index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const cors= require("cors");
+require("dotenv").config();
+const Transaction = require("./models/Transaction");
+const mongoose = require("mongoose");
+const app  = express();
+
+//nandandebanjana64
+//zNRbFIakzbdabfbB
+
+app.use(cors());
+app.use(express.json());
+app.get("/api/test",(req,res) => {
+    res.send({body : "testing successfull"});
+
+});
+
+app.post("/api/transaction", async(req, res) => {
+    //console.log(process.env.MONGO_URL);
+    await mongoose.connect(process.env.MONGO_URL);
+    const { name, description, datetime, price } = req.body;
+    const transaction = await Transaction.create({ 
+        name, 
+        description, 
+        datetime, 
+        price,
+     });
+
+    res.json(transaction);
+});
+
+app.get("/api/transaction", async (req, res) => {
+    await mongoose.connect(process.env.MONGO_URL);
+    const transactions = await Transaction.find();
+    res.json(transactions);
+  });
+
+app.listen(3001, () => {
+    console.log("Server is running on port 3001");
+});
